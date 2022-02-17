dafb6deefcf65596639c0c21a6783940dedc0bcd
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-02-17 19:46:44 +0000

    feat: add arima experiments using different models

---
+++ experiments/arima/src/evaluation.py
@@ -0,0 +1,50 @@
+from itertools import product
+
+import numpy as np
+import pandas as pd
+from neuralforecast.data.datasets.m4 import M4Evaluation, M4Info
+
+from src.data import get_data
+
+
+def evaluate(lib: str, group: str):
+    if lib != 'arima-r':
+        try:
+            forecast = pd.read_csv(f'data/{lib}-forecasts-M4-{group}.csv')
+        except:
+            return None
+        horizon = M4Info[group].horizon
+        if lib == 'statsforecast':
+            col = 'auto_arima_nixtla'
+        elif lib == 'prophet':
+            col = 'prophet'
+        else:
+            col = 'auto_arima_pmdarima'
+        forecast = forecast[col].values.reshape(-1, horizon)
+    else:
+        try:
+            forecast = np.loadtxt(f'data/{lib}-forecasts-M4-{group}.txt')
+        except:
+            return None
+    evals = M4Evaluation.evaluate('data', group, forecast)
+    times = pd.read_csv(f'data/{lib}-time-M4-{group}.csv')
+    evals = evals.rename_axis('dataset').reset_index()
+    evals = pd.concat([evals, times], axis=1)
+
+    return evals
+
+
+if __name__ == '__main__':
+    groups = ['Weekly', 'Hourly', 'Daily']
+    lib = ['statsforecast', 'arima-r', 'pmdarima', 'prophet']
+    evaluation = [evaluate(lib, group) for lib, group in product(lib, groups)]
+    evaluation = [eval_ for eval_ in evaluation if eval_ is not None]
+    evaluation = pd.concat(evaluation).sort_values(['dataset', 'model']).reset_index(drop=True)
+    evaluation = evaluation[['dataset', 'model', 'MASE', 'time']]
+    evaluation['time'] /= 60 #minutes
+    evaluation = evaluation.set_index(['dataset', 'model']).stack().reset_index()
+    evaluation.columns = ['dataset', 'model', 'metric', 'val']
+    evaluation = evaluation.set_index(['dataset', 'metric', 'model']).unstack().round(2)
+    evaluation = evaluation.droplevel(0, 1).reset_index()
+    evaluation.to_csv('data/m4-evaluation.csv')
+    print(evaluation.to_markdown(index=False))
---
+++ experiments/arima/src/statsforecast.py
@@ -0,0 +1,39 @@
+import time
+from functools import partial
+from multiprocessing import cpu_count
+
+import fire
+import numpy as np
+import pandas as pd
+from statsforecast import StatsForecast
+from statsforecast.models import auto_arima as auto_arima_nixtla
+
+from src.data import get_data
+
+
+def main(dataset: str = 'M3', group: str = 'Other') -> None:
+    train, horizon, freq, seasonality = get_data('data/', dataset, group)
+    train['ds'] = pd.to_datetime(train['ds']) 
+    train = train.set_index('unique_id')
+    
+    models = [
+        (auto_arima_nixtla, seasonality)
+    ]
+
+    start = time.time()
+    fcst = StatsForecast(train, models=models, freq=freq, n_jobs=cpu_count())
+    fcst.last_dates = pd.DatetimeIndex(fcst.last_dates)
+    forecasts = fcst.forecast(horizon)
+    end = time.time()
+    print(end - start)
+
+    forecasts = forecasts.reset_index()
+    forecasts.columns = ['unique_id', 'ds', 'auto_arima_nixtla']
+    forecasts.to_csv(f'data/statsforecast-forecasts-{dataset}-{group}.csv', index=False)
+
+    time_df = pd.DataFrame({'time': [end - start], 'model': ['auto_arima_nixtla']})
+    time_df.to_csv(f'data/statsforecast-time-{dataset}-{group}.csv', index=False)
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
