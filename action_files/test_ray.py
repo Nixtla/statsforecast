f85efecb59e53b4f31323e2b62249916031e93d4
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-05-01 19:56:29 -0500

    refactor: test_ray module

--- action_files/test_ray.py
+++ action_files/test_ray.py
@@ -19,20 +19,26 @@ from statsforecast.models import (
 )
 from statsforecast.utils import generate_series
 
-
-if __name__ == "__main__":
+def test_ray_flow():
+    n_series = 20
+    horizon = 7
+    models = [
+        adida, croston_classic, croston_optimized,
+        croston_sba, historic_average, imapa, naive, 
+        random_walk_with_drift, (seasonal_exponential_smoothing, 7, 0.1),
+        (seasonal_naive, 7), (seasonal_window_average, 7, 4),
+        (ses, 0.1), (tsb, 0.1, 0.3), (window_average, 4)
+    ]
     series = generate_series(20)
     ray_context = ray.init()
     fcst = StatsForecast(
-	series,
-	[adida, croston_classic, croston_optimized,
-	 croston_sba, historic_average, imapa, naive, 
-	 random_walk_with_drift, (seasonal_exponential_smoothing, 7, 0.1),
-	 (seasonal_naive, 7), (seasonal_window_average, 7, 4),
-	 (ses, 0.1), (tsb, 0.1, 0.3), (window_average, 4)],
-	freq='D',
-        n_jobs=int(ray.cluster_resources()['CPU']),
-	ray_address=ray_context.address_info['address']
+        series,
+        models=models,
+        freq='D',
+        n_jobs=-1,
+        ray_address=ray_context.address_info['address']
     )
-    fcst.forecast(7)
+    forecast = fcst.forecast(7)
     ray.shutdown()
+    assert forecast.shape == (n_series * horizon, len(models) + 1)
+
