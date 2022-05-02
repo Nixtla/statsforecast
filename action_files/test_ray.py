cb561ffb7123cb356283089ac98c3df38315655c
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-05-01 20:24:39 -0500

    feat: add test for get n jobs ray

--- action_files/test_ray.py
+++ action_files/test_ray.py
@@ -1,5 +1,8 @@
+from os import cpu_count
+
+import pytest
 import ray
-from statsforecast.core import StatsForecast
+from statsforecast.core import StatsForecast, _get_n_jobs
 from statsforecast.models import (
     adida,
     auto_arima,
@@ -19,6 +22,21 @@ from statsforecast.models import (
 )
 from statsforecast.utils import generate_series
 
+@pytest.mark.parametrize(
+	'test_input, expected', 
+	[
+		("(10, -1, 'auto')", cpu_count()), 
+		("(10, None, 'auto')", cpu_count()),
+		("(1, -1, 'auto')", 1),
+		("(1, None, 'auto')", 1),
+		("(2, 10, 'auto')", 2),
+	]
+)
+def test_ray_n_jobs(test_input, expected):
+	ray.init(ignore_reinit_error=True)
+	assert _get_n_jobs(*eval(test_input)) == expected
+	ray.shutdown()
+
 def test_ray_flow():
     n_series = 20
     horizon = 7
@@ -30,7 +48,7 @@ def test_ray_flow():
         (ses, 0.1), (tsb, 0.1, 0.3), (window_average, 4)
     ]
     series = generate_series(20)
-    ray_context = ray.init()
+    ray_context = ray.init(ignore_reinit_error=True)
     fcst = StatsForecast(
         series,
         models=models,
