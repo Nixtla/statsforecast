d3a6a051b0588de0c98f1ef3e851c0aa201d1191
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-06-07 18:19:49 -0500

    feat: complete mstl function (multiple seasonalities)

--- statsforecast/mstl.py
+++ statsforecast/mstl.py
@@ -3,15 +3,20 @@
 __all__ = []
 
 # Cell
+from typing import List, Optional, Union
+
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 
+from .utils import AirPassengers as ap
+
 # Internal Cell
-def mstl(x, period, blambda=None, s_window=7 + 4 * np.arange(1, 7)):
+def mstl(x: np.ndarray, period: Union[int, List[int]], blambda: Optional[float] = None,
+         iterate: int = 1, s_window: np.ndarray = 7 + 4 * np.arange(1, 7)):
     origx = x
     n = len(x)
-    msts = period
+    msts = [period] if isinstance(period, int) else period
     iterate = 1
     if x.ndim == 2:
         x = x[:, 0]
@@ -20,10 +25,17 @@ def mstl(x, period, blambda=None, s_window=7 + 4 * np.arange(1, 7)):
     if blambda is not None:
         ...  # boxcox
     tt = np.arange(n)
-    if msts > 1:
-        fit = sm.tsa.STL(x, period=msts, seasonal=s_window[0]).fit()
-        seas = fit.seasonal
-        deseas = x - seas
+    if msts[0] > 1:
+        seas = np.zeros((len(msts), n))
+        deseas = np.copy(x)
+        if len(s_window) == 1:
+            s_window = np.repeat(s_window, len(msts))
+        for j in range(iterate):
+            for i, seas_ in enumerate(msts, start=0):
+                deseas = deseas + seas[i]
+                fit = sm.tsa.STL(deseas, period=seas_, seasonal=s_window[i]).fit()
+                seas[i] = fit.seasonal
+                deseas = deseas - seas[i]
         trend = fit.trend
     else:
         try:
@@ -39,6 +51,10 @@ def mstl(x, period, blambda=None, s_window=7 + 4 * np.arange(1, 7)):
     remainder = deseas - trend
     output = {'data': origx, 'trend': trend}
     if msts is not None:
-        output['seasonal'] = seas
+        if len(msts) == 1:
+            output['seasonal'] = seas[0]
+        else:
+            for i, seas_ in enumerate(msts, start=0):
+                output[f'seasonal{seas_}'] = seas[i]
     output['remainder'] = remainder
     return pd.DataFrame(output)
