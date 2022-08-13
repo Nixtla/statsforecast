1391c67cbf8b77480d3fb54406769d53298adf09
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-08-12 22:50:45 -0500

    fix: add encoding setup

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
     extras_require={'dev': dev_requirements, 'ray': ray_requirements, 'fugue': fugue_requirements,},
     dependency_links = cfg.get('dep_links','').split(),
     python_requires  = '>=' + cfg['min_python'],
-    long_description = open('README.md').read(),
+    long_description = open('README.md', encoding='utf8').read(),
     long_description_content_type = 'text/markdown',
     zip_safe = False,
     entry_points = {
