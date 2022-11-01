ac70f9f5cb5d66127bc494d805ef1fb6bf404c6c
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-11-01 00:26:28 +0000

    feat: add cross val fallback to distributed

--- statsforecast/distributed/core.py
+++ statsforecast/distributed/core.py
@@ -14,6 +14,6 @@ class ParallelBackend:
         model = StatsForecast(df=df, models=models, freq=freq, fallback_model=fallback_model)
         return model.forecast(**kwargs)
 
-    def cross_validation(self, df, models, freq, **kwargs: Any) -> Any:
-        model = StatsForecast(df=df, models=models, freq=freq)
+    def cross_validation(self, df, models, freq, fallback_model=None, **kwargs: Any) -> Any:
+        model = StatsForecast(df=df, models=models, freq=freq, fallback_model=fallback_model)
         return model.cross_validation(**kwargs)
