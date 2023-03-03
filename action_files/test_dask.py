0512e7dc1cc00646ebb031052c6fae6641a066f4
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-03-03 15:34:46 -0600

    fix: rename dask test

--- action_files/test_dask.py
+++ action_files/test_dask.py
@@ -3,7 +3,7 @@ import dask.dataframe as dd
 from statsforecast.utils import generate_series
 from .utils import pipeline
 
-def test_ray_flow():
+def test_dask_flow():
     n_series = 2
     horizon = 7
     series = generate_series(n_series).reset_index()
