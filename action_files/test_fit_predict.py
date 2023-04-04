81ece26e5f324831246152e65407554d0d0ce017
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-04-04 00:05:19 +0000

    [ci]: parametriz fit/predict tests

--- action_files/test_fit_predict.py
+++ action_files/test_fit_predict.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from neuralforecast.utils import AirPassengersPanel
 from statsforecast import StatsForecast
@@ -12,8 +13,8 @@ from statsforecast.models import (
     TSB
 )
 
-
-def test_fit_predict_parallel():
+@pytest.mark.parametrize('n_jobs', [-1, 1])
+def test_fit_predict(n_jobs):
     models = [
         AutoARIMA(),
         AutoETS(),
@@ -35,36 +36,7 @@ def test_fit_predict_parallel():
         TSB(0.5, 0.5)
     ]
         
-    sf = StatsForecast(models=models, freq='D', n_jobs=-1)
-    df_fcst = sf.forecast(df=AirPassengersPanel[['unique_id', 'ds', 'y']], h=7)
-    sf.fit(df=AirPassengersPanel[['unique_id', 'ds', 'y']])
-    df_predict = sf.predict(h=7)
-    pd.testing.assert_frame_equal(df_fcst, df_predict)
-
-
-def test_fit_predict_sequential():
-    models = [
-        AutoARIMA(),
-        AutoETS(),
-        AutoCES(),
-        AutoTheta(),
-        MSTL(season_length=12),
-        GARCH(),
-        ARCH(), 
-        HistoricAverage(),
-        Naive(),
-        RandomWalkWithDrift(),
-        SeasonalNaive(season_length=12),
-        WindowAverage(window_size=12),
-        SeasonalWindowAverage(window_size=12, season_length=12),
-        ADIDA(),
-        CrostonClassic(),
-        CrostonOptimized(),
-        IMAPA(),
-        TSB(0.5, 0.5)
-    ]
-        
-    sf = StatsForecast(models=models, freq='D')
+    sf = StatsForecast(models=models, freq='D', n_jobs=n_jobs)
     df_fcst = sf.forecast(df=AirPassengersPanel[['unique_id', 'ds', 'y']], h=7)
     sf.fit(df=AirPassengersPanel[['unique_id', 'ds', 'y']])
     df_predict = sf.predict(h=7)
