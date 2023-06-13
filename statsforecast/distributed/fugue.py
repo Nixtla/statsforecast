0f57808680b3ef2039014d337a39d719bb7c6254
Kevin Kho <kdykho@gmail.com>
2023-06-13 18:21:26 -0500

    [DOCS] Updating Distributed Documentation (#541)
    * Updating distributed documentation
    
    * Cleaned notebooks
    
    * Removing changes to sidebar

--- statsforecast/distributed/fugue.py
+++ statsforecast/distributed/fugue.py
@@ -227,5 +227,4 @@ class FugueBackend(ParallelBackend):
 
 @make_backend.candidate(lambda obj, *args, **kwargs: isinstance(obj, ExecutionEngine))
 def _make_fugue_backend(obj: ExecutionEngine, *args, **kwargs) -> ParallelBackend:
-    print("make fugue")
     return FugueBackend(obj, **kwargs)
