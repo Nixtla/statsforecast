86066293e436383d96724326979f7b1993915e09
kvnkho <kdykho@gmail.com>
2023-07-09 00:03:46 -0500

    Adding tests

--- action_files/test_dask.py
+++ action_files/test_dask.py
@@ -1,12 +1,24 @@
 import dask.dataframe as dd
+import pytest
 
 from statsforecast.utils import generate_series
-from .utils import pipeline
+from .utils import pipeline, pipeline_with_level
 
-def test_dask_flow():
-    n_series = 2
-    horizon = 7
+@pytest.fixture()
+def n_series():
+    return 2
+
+@pytest.fixture()
+def sample_data(n_series):
     series = generate_series(n_series).reset_index()
     series['unique_id'] = series['unique_id'].astype(str)
     series = dd.from_pandas(series, npartitions=2)
-    pipeline(series, n_series, horizon)
+    return series
+
+def test_dask_flow(sample_data, n_series):
+    horizon = 7
+    pipeline(sample_data, n_series, horizon)
+
+def test_dask_flow_with_level(sample_data, n_series):
+    horizon = 7
+    pipeline_with_level(sample_data, n_series, horizon)
