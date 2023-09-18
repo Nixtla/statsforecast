689a1365f47c86b0b0a483e375328ca8cbabffaa
José Morales <jmoralz92@gmail.com>
2023-09-18 13:15:35 -0600

    skip ray test on py < 3.8

--- action_files/test_ray.py
+++ action_files/test_ray.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 import ray
 
@@ -12,8 +14,10 @@ def sample_data(local_data):
     series, X_df = local_data
     return to_distributed(series), to_distributed(X_df)
 
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python >= 3.8")
 def test_ray_flow(horizon, sample_data, n_series):
     pipeline(*sample_data, n_series, horizon)
 
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python >= 3.8")
 def test_ray_flow_with_level(horizon, sample_data, n_series):
     pipeline_with_level(*sample_data, n_series, horizon)
