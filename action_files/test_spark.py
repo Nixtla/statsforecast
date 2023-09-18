a69013ea4d0a84034a0c14e487f808ab089011af
José Morales <jmoralz92@gmail.com>
2023-09-18 16:58:24 -0600

    skip spark test on py < 3.8

--- action_files/test_spark.py
+++ action_files/test_spark.py
@@ -16,8 +16,10 @@ def sample_data(spark, local_data):
     series, X_df = local_data
     return to_distributed(spark, series), to_distributed(spark, X_df)
 
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python >= 3.8")
 def test_spark_flow(horizon, sample_data, n_series):
     pipeline(*sample_data, n_series, horizon)
 
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python >= 3.8")
 def test_spark_flow_with_level(horizon, sample_data, n_series):
     pipeline_with_level(*sample_data, n_series, horizon)
