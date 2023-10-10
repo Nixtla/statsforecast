20b961bd183cf2d0572dc27c9bcf32985d8f3a83
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2023-10-10 12:41:28 -0700

    add ostype if else

--- final-formatting.bash
+++ final-formatting.bash
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 for file in $(find _docs -type f -name "*mdx"); do
-  echo ${file}
-  sed -i '' -e 's/style="float:right; font-size:smaller"/style={{ float: "right", fontSize: "smaller" }}/g' $file
-  sed -i '' -e 's/<br>/<br\/>/g' $file
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e 's/style="float:right; font-size:smaller"/style={{ float: "right", fontSize: "smaller" }}/g' $file
+    sed -i '' -e 's/<br>/<br\/>/g' $file
+  else
+    sed -i -e 's/style="float:right; font-size:smaller"/style={{ float: "right", fontSize: "smaller" }}/g' $file
+    sed -i -e 's/<br>/<br\/>/g' $file
+  fi
 done
