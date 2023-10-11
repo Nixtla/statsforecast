5a7418d3deb681df35b03b11092963595474e35a
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2023-10-10 22:22:04 -0700

    delete contribute

--- preview-mintlify-docs.bash
+++ preview-mintlify-docs.bash
@@ -14,13 +14,6 @@ cp nbs/mint.json _docs/mint.json
 cp nbs/imgs/logo/dark.png _docs/dark.png
 cp nbs/imgs/logo/light.png _docs/light.png 
 cp nbs/favicon.svg _docs/favicon.svg
-mkdir _docs/docs/contribute
-cp _proc/docs/contribute/contribute.md _docs/docs/contribute/contribute.mdx
-cp _proc/docs/contribute/docs.md _docs/docs/contribute/docs.mdx
-cp _proc/docs/contribute/issue-labels.md _docs/docs/contribute/issue-labels.mdx
-cp _proc/docs/contribute/issues.md _docs/docs/contribute/issues.mdx
-cp _proc/docs/contribute/step-by-step.md _docs/docs/contribute/step-by-step.mdx
-cp _proc/docs/contribute/techstack.md _docs/docs/contribute/techstack.mdx
 echo "Done moving necessary assets"
 
 cd ./_docs
