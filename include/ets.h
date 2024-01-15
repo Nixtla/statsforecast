3c3090edc58c94ed38405b4c2aafcac4c95ec667
José Morales <jmoralz92@gmail.com>
2024-01-15 11:37:37 -0600

    add export for windows

--- include/ets.h
+++ include/ets.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "export.h"
 #include "nelder_mead.h"
 
 enum class Component {
@@ -19,24 +20,25 @@ constexpr double NA = -99999.0;
 constexpr double TOL = 1e-10;
 
 extern "C" {
-void ETS_Update(double &l, double &b, double *s, double old_l, double old_b,
-                const double *old_s, int m, Component trend, Component season,
-                double alpha, double beta, double gamma, double phi, double y);
+DLL_EXPORT void ETS_Update(double &l, double &b, double *s, double old_l,
+                           double old_b, const double *old_s, int m,
+                           Component trend, Component season, double alpha,
+                           double beta, double gamma, double phi, double y);
 
-void ETS_Forecast(double l, double b, const double *s, int m, Component trend,
-                  Component season, double phi, double *f, int h);
+DLL_EXPORT void ETS_Forecast(double l, double b, const double *s, int m,
+                             Component trend, Component season, double phi,
+                             double *f, int h);
 
-double ETS_Calc(const double *y, size_t n, double *x, Component error,
-                Component trend, Component season, double alpha, double beta,
-                double gamma, double phi, double *e, double *a_mse, int n_mse,
-                int m);
+DLL_EXPORT double ETS_Calc(const double *y, size_t n, double *x,
+                           Component error, Component trend, Component season,
+                           double alpha, double beta, double gamma, double phi,
+                           double *e, double *a_mse, int n_mse, int m);
 
-OptimResult ETS_NelderMead(double *x0, size_t n_x0, const double *y, size_t n_y,
-                           int n_state, Component error, Component trend,
-                           Component season, Criterion opt_crit, int n_mse,
-                           int m, bool opt_alpha, bool opt_beta, bool opt_gamma,
-                           bool opt_phi, double alpha, double beta,
-                           double gamma, double phi, const double *lower,
-                           const double *upper, double tol_std, int max_iter,
-                           bool adaptive);
+DLL_EXPORT OptimResult ETS_NelderMead(
+    double *x0, size_t n_x0, const double *y, size_t n_y, int n_state,
+    Component error, Component trend, Component season, Criterion opt_crit,
+    int n_mse, int m, bool opt_alpha, bool opt_beta, bool opt_gamma,
+    bool opt_phi, double alpha, double beta, double gamma, double phi,
+    const double *lower, const double *upper, double tol_std, int max_iter,
+    bool adaptive);
 }
---
+++ include/export.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifdef _MSC_VER
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
