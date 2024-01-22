5aa05ddece64204601597a1ed5d8ca08f7a3adff
José Morales <jmoralz92@gmail.com>
2024-01-22 17:00:01 -0600

    lint2

--- statsforecast/mstl.py
+++ statsforecast/mstl.py
@@ -4,7 +4,7 @@
 __all__ = ['mstl']
 
 # %% ../nbs/src/mstl.ipynb 3
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional
 
 import numpy as np
 import pandas as pd
