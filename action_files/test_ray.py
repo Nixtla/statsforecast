2bac6dbd6e1ada75be1c1b3e45914e368da38da6
José Morales <jmoralz92@gmail.com>
2024-06-11 13:02:17 -0600

    skip complete file

--- action_files/test_ray.py
+++ action_files/test_ray.py
@@ -1,6 +1,9 @@
 import sys
 
 import pytest
+
+if sys.version_info >= (3, 12):
+    pytest.skip("Ray requires Python<3.12", allow_module_level=True)
 import ray
 
 from .utils import pipeline, pipeline_with_level, pipeline_fitted
@@ -16,23 +19,14 @@ def sample_data(local_data):
     return to_distributed(series), to_distributed(X_df)
 
 
-@pytest.mark.skipif(
-    sys.version_info >= (3, 12), reason="ray does not support python 3.12"
-)
 def test_ray_flow(horizon, sample_data, n_series):
     pipeline(*sample_data, n_series, horizon)
 
 
-@pytest.mark.skipif(
-    sys.version_info >= (3, 12), reason="ray does not support python 3.12"
-)
 def test_ray_flow_with_level(horizon, sample_data, n_series):
     pipeline_with_level(*sample_data, n_series, horizon)
 
 
-@pytest.mark.skipif(
-    sys.version_info >= (3, 12), reason="ray does not support python 3.12"
-)
 @pytest.mark.parametrize("use_x", [True, False])
 def test_ray_flow_with_fitted(horizon, use_x, sample_data):
     series, X_df = sample_data
