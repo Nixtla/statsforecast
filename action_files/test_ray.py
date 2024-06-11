523f3dfb14a7f0dbc051b0e64cfb25c3c4896b44
José Morales <jmoralz92@gmail.com>
2024-06-11 12:40:15 -0600

    skip ray tests on 3.12

--- action_files/test_ray.py
+++ action_files/test_ray.py
@@ -9,20 +9,31 @@ from .utils import pipeline, pipeline_with_level, pipeline_fitted
 def to_distributed(df):
     return ray.data.from_pandas(df).repartition(2)
 
+
 @pytest.fixture()
 def sample_data(local_data):
     series, X_df = local_data
     return to_distributed(series), to_distributed(X_df)
 
-@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python >= 3.8")
+
+@pytest.mark.skipif(
+    sys.version_info >= (3, 12), reason="ray does not support python 3.12"
+)
 def test_ray_flow(horizon, sample_data, n_series):
     pipeline(*sample_data, n_series, horizon)
 
-@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python >= 3.8")
+
+@pytest.mark.skipif(
+    sys.version_info >= (3, 12), reason="ray does not support python 3.12"
+)
 def test_ray_flow_with_level(horizon, sample_data, n_series):
     pipeline_with_level(*sample_data, n_series, horizon)
 
-@pytest.mark.parametrize('use_x', [True, False])
+
+@pytest.mark.skipif(
+    sys.version_info >= (3, 12), reason="ray does not support python 3.12"
+)
+@pytest.mark.parametrize("use_x", [True, False])
 def test_ray_flow_with_fitted(horizon, use_x, sample_data):
     series, X_df = sample_data
     if not use_x:
