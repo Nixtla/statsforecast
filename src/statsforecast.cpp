4bd43641eecd8c87c7157fc1c39f9cc5528eef68
José Morales <jmoralz92@gmail.com>
2024-09-03 16:57:39 -0600

    feat: migrate arima to c++ (#895)

--- src/statsforecast.cpp
+++ src/statsforecast.cpp
@@ -2,10 +2,18 @@
 
 namespace py = pybind11;
 
-namespace ets {
-void init(py::module_ &);
+namespace ets
+{
+  void init(py::module_ &);
 }
 
-PYBIND11_MODULE(_lib, m) {
+namespace arima
+{
+  void init(py::module_ &);
+}
+
+PYBIND11_MODULE(_lib, m)
+{
+  arima::init(m);
   ets::init(m);
 }
