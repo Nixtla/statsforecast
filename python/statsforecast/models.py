6d6b52f2451fe0c8e431dd505c254ec4c9de0b0d
gunnvant <gunnvant3.14@gmail.com>
2025-02-05 23:06:17 +0530

    Running pre-commit hooks

--- python/statsforecast/models.py
+++ python/statsforecast/models.py
@@ -4526,7 +4526,7 @@ class CrostonClassic(_TS):
         Returns
         -------
         forecasts : dict
-            Dictionary with entries `mean` for point predictions and `level_*` for probabilistic predictions.
+            Dictionary with entries `mean` for point predictions and `level_*` for probabilistic predictions
         """
         mean = _repeat_val(val=self.model_["mean"][0], h=h)
         res = {"mean": mean}
@@ -4932,7 +4932,7 @@ class CrostonSBA(_TS):
         Returns
         -------
         forecasts : dict
-            Dictionary with entries `fitted` for point predictions and `level_*` for probabilistic predictions.
+            Dictionary with entries `mean` for point predictions and `level_*` for probabilistic predictions.
         """
         res = {"fitted": self.model_["fitted"]}
         if level is not None:
