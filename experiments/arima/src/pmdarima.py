a0cd137756e96248e0145b6946c6dd6548676988
Saul <saul.caballero.ramirez@gmail.com>
2025-07-14 20:19:14 -0600

    Correctly define the pmdarima comparison

--- experiments/arima/src/pmdarima.py
+++ experiments/arima/src/pmdarima.py
@@ -7,43 +7,43 @@ import fire
 import numpy as np
 import pandas as pd
 from pmdarima.arima import auto_arima
-from statsforecast.models import random_walk_with_drift
 
 from src.data import get_data
 
 
 def fit_and_predict(index, ts, horizon, freq, seasonality): 
     x = ts['y'].values
-    try:
-        mod = auto_arima(x, m=seasonality,
-                         with_intercept=False,
-                         error_action='ignore')
-        forecast = mod.predict(horizon)
-    except:
-        forecast = random_walk_with_drift(x, horizon)
-
-    forecast = pd.DataFrame({
-        'ds': np.arange(ts['ds'].max() + 1, ts['ds'].max() + horizon + 1),
-        'ypred': forecast
-    })
-    forecast['unique_id'] = index
-    
-    return forecast[['unique_id', 'ds', 'ypred']]
+    mod = auto_arima(x, m=seasonality, with_intercept=False, error_action='ignore')
+    forecast = mod.predict(horizon)
+    return forecast, index, ts
 
 def main(dataset: str, group: str) -> None:
     train, horizon, freq, seasonality = get_data('data', dataset, group)
-    
-    partial_fit_and_predict = partial(fit_and_predict, 
-                                      horizon=horizon, freq=freq, seasonality=seasonality)
+    print(train)
+    partial_fit_and_predict = partial(
+        fit_and_predict,
+        horizon=horizon,
+        freq=freq,
+        seasonality=seasonality
+    )
     start = time.time()
     print(f'Parallelism on {cpu_count()} CPU')
     with Pool(cpu_count()) as pool:
         results = pool.starmap(partial_fit_and_predict, train.groupby('unique_id'))
     end = time.time()
     print(end - start)
+
+    ds = pd.date_range(start=train.ds.max(), periods=horizon + 1, freq=freq)[1:]
+    preds = []
+    for pred in results:
+        preds.append(
+            pd.DataFrame({
+            "unique_id": pred[1],
+            "ds": ds,
+            "auto_arima_pmdarima": pred[0],
+        }))
     
-    forecasts = pd.concat(results)
-    forecasts.columns = ['unique_id', 'ds', 'auto_arima_pmdarima']
+    forecasts = pd.concat(preds)
     forecasts.to_csv(f'data/pmdarima-forecasts-{dataset}-{group}.csv', index=False)
 
     time_df = pd.DataFrame({'time': [end - start], 'model': ['auto_arima_pmdarima']})
