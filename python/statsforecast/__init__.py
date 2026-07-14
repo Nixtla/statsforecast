637da41d8861630b1790eae865b477838a9fb010
Saul Caballero <saul.caballero.ramirez@gmail.com>
2026-07-14 16:05:48 -0600

    Bumps version and updates dynamic versioning

--- python/statsforecast/__init__.py
+++ python/statsforecast/__init__.py
@@ -1,4 +1,6 @@
-__version__ = "2.0.3"
+from importlib.metadata import version
+
+__version__ = version("statsforecast")
 __all__ = ["StatsForecast"]
 from .core import StatsForecast
 from .distributed import fugue  # noqa
