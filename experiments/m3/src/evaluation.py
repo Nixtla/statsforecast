8dbf728ef343c9ff2486b92e64467a7bdc0e1d59
Jose <jmoralz92@gmail.com>
2026-09-02 14:46:37 -0600

    restore expected

--- experiments/m3/src/evaluation.py
+++ experiments/m3/src/evaluation.py
@@ -81,7 +81,7 @@ def main(test: bool = False):
     time = evaluation.query('metric=="time"').T
     if test:
         expected_results = {
-            "AutoARIMA": 4.43,
+            "AutoARIMA": 4.46,
             "CES": 4.85,
             "AutoETS": 4.35,
             "DynamicOptimizedTheta": 4.54,
